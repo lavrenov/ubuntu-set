f385f71517e098a35659821e7853b03d0771f05a
EvgenyLavr <lion06@inbox.ru>
2021-10-21 13:02:32 +0300

    fix check error

--- install.sh
+++ install.sh
@@ -11,36 +11,36 @@ then
   exit 0
 fi
 
-echo "===========================================" >> "$LOG"
-date +"Start install - %F %T" >> "$LOG"
-echo "===========================================" >> "$LOG"
+echo "===========================================" &>> "$LOG"
+date +"Start install - %F %T" &>> "$LOG"
+echo "===========================================" &>> "$LOG"
 
 echo "Remove unused applications" | tee -a "$LOG"
-./$UTILS_DIR/remove-unused.sh >> $LOG
+./$UTILS_DIR/remove-unused.sh &>> $LOG
 
 echo "Update && upgrade" | tee -a "$LOG"
-./$UTILS_DIR/update.sh >> $LOG
+./$UTILS_DIR/update.sh &>> $LOG
 
 echo "Fix audio" | tee -a "$LOG"
-./$UTILS_DIR/fix-audio.sh >> $LOG
+./$UTILS_DIR/fix-audio.sh &>> $LOG
 
 echo "Install base applications" | tee -a "$LOG"
-./$UTILS_DIR/base.sh >> $LOG
+./$UTILS_DIR/base.sh &>> $LOG
 
 echo "Install applications from $APPS_DIR dir" | tee -a "$LOG"
 APPS=`ls $APPS_DIR`
 for APP in ${APPS[@]}
 do
   echo "Install $APP" | tee -a "$LOG"
-  ./$APPS_DIR/$APP >> $LOG
+  ./$APPS_DIR/$APP &>> $LOG
 done
 
 echo "Clean" | tee -a "$LOG"
-./$UTILS_DIR/clean.sh >> $LOG
+./$UTILS_DIR/clean.sh &>> $LOG
 
-echo "===========================================" >> "$LOG"
-date +"Finished - %F %T" >> "$LOG"
-echo "===========================================" >> "$LOG"
+echo "===========================================" &>> "$LOG"
+date +"Finished - %F %T" &>> "$LOG"
+echo "===========================================" &>> "$LOG"
 
 errors=`cat "$LOG" | grep -E "E:|error:"`
 if [[ -n "${errors}" ]]; then
