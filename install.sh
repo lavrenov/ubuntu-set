60868cdce17824ebc3123c3d019ec987c7cf91b2
EvgenyLavr <lion06@inbox.ru>
2021-11-20 12:05:58 +0300

    fix error log

--- install.sh
+++ install.sh
@@ -16,27 +16,27 @@ date +"Start install - %F %T" &>> "$LOG"
 echo "===========================================" &>> "$LOG"
 
 echo "Remove unused applications" | tee -a "$LOG"
-./$UTILS_DIR/remove-unused.sh &>> $LOG
+./$UTILS_DIR/remove-unused.sh 2>> $LOG
 
 echo "Update && upgrade" | tee -a "$LOG"
-./$UTILS_DIR/update.sh &>> $LOG
+./$UTILS_DIR/update.sh 2>> $LOG
 
 echo "Fix audio" | tee -a "$LOG"
-./$UTILS_DIR/fix-audio.sh &>> $LOG
+./$UTILS_DIR/fix-audio.sh 2>> $LOG
 
 echo "Install base applications" | tee -a "$LOG"
-./$UTILS_DIR/base.sh &>> $LOG
+./$UTILS_DIR/base.sh 2>> $LOG
 
 echo "Install applications from $APPS_DIR dir" | tee -a "$LOG"
 APPS=`ls $APPS_DIR`
 for APP in ${APPS[@]}
 do
   echo "Install $APP" | tee -a "$LOG"
-  ./$APPS_DIR/$APP &>> $LOG
+  ./$APPS_DIR/$APP 2>> $LOG
 done
 
 echo "Clean" | tee -a "$LOG"
-./$UTILS_DIR/clean.sh &>> $LOG
+./$UTILS_DIR/clean.sh 2>> $LOG
 
 echo "===========================================" &>> "$LOG"
 date +"Finished - %F %T" &>> "$LOG"
